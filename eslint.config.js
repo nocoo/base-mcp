ad2fbda7e03a009e2579e8f8ec9553b800f421ce
nocoo <niconicoconi@gmail.com>
2026-04-11 08:39:24 +0800

    ci: add CI workflow and fix lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "error",
-        { argsIgnorePattern: "^_" },
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
       "@typescript-eslint/no-explicit-any": "warn",
     },
